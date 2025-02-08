78727c2bc74ec18a9cf0bbe0570a8e07b8a69024
ksavery77 <ksawery.klimowski@gmail.com>
2025-02-08 11:12:40 +0100

    Create LessonScreen.js

---
+++ frontend/screens/LessonScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function LessonScreen() {
+  return (
+    <View>
+      <Text>Lesson content goes here</Text>
+    </View>
+  );
+}
